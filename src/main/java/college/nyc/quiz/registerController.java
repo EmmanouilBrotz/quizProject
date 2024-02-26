7e8373e2ea1dcb3a4458d7c26fd5d72bc83b72ff
Rafail  Brotzakis <rbrotzakis@nyc.gr>
2024-02-26 20:10:14 +0200

    Started working on Register and Controllers

--- src/main/java/college/nyc/quiz/registerController.java
+++ src/main/java/college/nyc/quiz/registerController.java
@@ -1,4 +1,22 @@
 package college.nyc.quiz;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class registerController {
+    @FXML
+    private Button registerUserButton;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    protected void registerUser(){ //Whenever the user inputs their credentials, they shall be imported to the database.
+
+    }
 }
